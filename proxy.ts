9674269b28f40c527347e917cde9ba3dc517ff93
TBL007 <tholut15@imkatta.no>
2026-05-29 14:33:57 +0200

    test av vercel

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function proxy(request: NextRequest) {
-  const token = await getToken({ req: request, secret: process.env.AUTH_SECRET, });
+  const token = await getToken({ req: request, secret: process.env.AUTH_SECRET });
 
   if (!token) {
     return NextResponse.redirect(new URL("/api/auth/signin", request.url));
